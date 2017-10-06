7f0c2517d677fff93711d33ad858aa7d2cf375de
Madamadakala, Ganapathi <Ganapathi.Madamadakala@capitalone.com>
2017-10-06 09:21:21 -0400

    trie implementation

---
+++ ganesh/src/com/basic/ganesh/sortingAlgorithms/TrieNode.java
@@ -0,0 +1,15 @@
+package com.basic.ganesh.sortingAlgorithms;
+
+import java.util.HashMap;
+
+public class TrieNode {
+	char c;
+	HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+	boolean isLeaf;
+
+	public TrieNode() {}
+
+	public TrieNode(char c){
+		this.c = c;
+	}
+}
---
+++ ganesh/src/com/basic/ganesh/sortingAlgorithms/Trie_impl_hash.java
@@ -0,0 +1,74 @@
+package com.basic.ganesh.sortingAlgorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Trie_impl_hash {
+
+	private TrieNode root;
+	public Trie_impl_hash() {
+		root = new TrieNode();
+	}
+	// Inserts a word into the trie.
+	public void insert(String word) {
+		HashMap<Character, TrieNode> children = root.children;
+		for(int i=0; i<word.length(); i++){
+			char c = word.charAt(i);
+			TrieNode t;
+			if(children.containsKey(c)){
+				t = children.get(c);
+			}else{
+				t = new TrieNode(c);
+				children.put(c, t);
+			}
+			children = t.children;
+			//set leaf node
+			if(i==word.length()-1)
+				t.isLeaf = true;
+		}
+	}
+	// Returns if the word is in the trie.
+	public boolean search(String word) {
+		TrieNode t = searchNode(word);
+		if(t != null && t.isLeaf)
+			return true;
+		else
+			return false;
+	}
+	// Returns if there is any word in the trie
+	// that starts with the given prefix.
+	public boolean startsWith(String prefix) {
+		if(searchNode(prefix) == null)
+			return false;
+		else
+			return true;
+	}
+	public TrieNode searchNode(String str){
+		Map<Character, TrieNode> children = root.children;
+		TrieNode t = null;
+		for(int i=0; i<str.length(); i++){
+			char c = str.charAt(i);
+			if(children.containsKey(c)){
+				t = children.get(c);
+				children = t.children;
+			}else{
+				return null;
+			}
+		}
+		return t;
+	}
+
+	// Driver program
+	public static void main(String args[])
+	{
+		Trie_impl_hash impl_hash = new Trie_impl_hash();
+		impl_hash.insert("to");
+		impl_hash.insert("tea");
+		impl_hash.insert("ted");
+		
+		System.out.println(impl_hash.search("to"));
+	}
+
+}
+

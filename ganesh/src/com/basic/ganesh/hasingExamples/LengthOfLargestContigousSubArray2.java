498c9138fe397ff76e97074a657e56397f73e82f
Ganapathi Madamadakala <neelu@Ganapathis-MacBook-Pro.local>
2017-12-29 01:06:04 -0500

    adding hash problems

---
+++ ganesh/src/com/basic/ganesh/hasingExamples/LengthOfLargestContigousSubArray2.java
@@ -0,0 +1,51 @@
+package com.basic.ganesh.hasingExamples;
+
+public class LengthOfLargestContigousSubArray2 {
+	
+	    // Utility functions to find minimum and maximum of
+	    // two elements
+	 
+	    static int min(int x, int y) 
+	    {
+	        return (x < y) ? x : y;
+	    }
+	 
+	    static int max(int x, int y) 
+	    {
+	        return (x > y) ? x : y;
+	    }
+	 
+	    // Returns length of the longest contiguous subarray
+	    static int findLength(int arr[], int n) 
+	    {
+	        int max_len = 1;  // Initialize result
+	        for (int i = 0; i < n - 1; i++) 
+	        {
+	            // Initialize min and max for all subarrays starting with i
+	            int mn = arr[i], mx = arr[i];
+	 
+	            // Consider all subarrays starting with i and ending with j
+	            for (int j = i + 1; j < n; j++) 
+	            {
+	                // Update min and max in this subarray if needed
+	                mn = min(mn, arr[j]);
+	                mx = max(mx, arr[j]);
+	 
+	                // If current subarray has all contiguous elements
+	                if ((mx - mn) == j - i)
+	                    max_len = max(max_len, mx - mn + 1);
+	            }
+	        }
+	        return max_len;  // Return result
+	    }
+	 
+	    public static void main(String[] args) 
+	    {
+	        int arr[] = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+	        int n = arr.length;
+	        System.out.println("Length of the longest contiguous subarray is "
+	                + findLength(arr, n));
+	    }
+	}
+	 
+	
